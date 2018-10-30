a0304b843e21b74b2b60586c9b88f743513f8482
Philip Conrad <philipaconrad@gmail.com>
2018-10-30 16:45:53 +0000

    Add tracing script.
    This commit adds a more advanced version of the Bash-based tracing
    script that has been used thus far. It requires several command line
    parameters for figuring out where stuff lives, but that's the only major
    wrinkle.
    
    It generates an LTTng tracing session around a user-specified command
    (assumed to be an OpenMP-based program).

---
+++ scripts/trace.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# --------------------------------------------------------
+# Shell script prelude
+
+USAGE=$(cat <<EOF
+Tracing script for use with Visuomp.
+Usage:
+    trace.sh TRACEFILE_DEST OMP_LIB VISUOMP_LIB ARGS...
+
+Arguments:
+  TRACEFILE_DEST    Where to write the LTTng traces.
+  OMP_LIB           OpenMP shared library to use with user application.
+  VISUOMP_LIB       Visuomp shared library to use with user application.
+
+Examples:
+    trace.sh /tmp/ompt-jacobi \\ 
+      /opt/openmp-install/lib/libomp.so \\ 
+      /opt/pinsight/lib/libvisuomp.so \\ 
+      ./jacobi 2048 2048
+EOF
+)
+
+# Check to make sure user provided enough args.
+# If not, then exit early with usage info.
+if [ $# -lt 4 ]; then
+    echo "Error: Not enough arguments provided.";
+    echo "${USAGE}";
+    exit 1;
+fi
+
+# Read in positional parameters, then shift array.
+# This leaves only the program command line in `$@`.
+TRACING_OUTPUT_DEST=$1
+OMP_LIB_PATH=$2
+VISUOMP_LIB_PATH=$3
+shift 3
+
+
+# --------------------------------------------------------
+# Main script
+
+# Create a userspace session.
+lttng create ompt-tracing-session --output="${TRACING_OUTPUT_DEST}"
+
+# Create and enable event rules.
+lttng enable-event --userspace lttng_visuomp:'*'
+
+# Start LTTng tracing.
+lttng start
+
+# Run instrumented code.
+LD_PRELOAD=${OMP_LIB_PATH}:${VISUOMP_LIB_PATH} "$@"
+
+# Stop LTTng tracing.
+lttng stop
+lttng destroy
+
