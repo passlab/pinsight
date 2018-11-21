0721603ead00f195642728a30985452f6f9d5c00
Philip Conrad <philipaconrad@gmail.com>
2018-11-21 03:01:36 -0500

    Fix environment variable queries.
    This commit fixes confusing behavior from the environment variable
    querying functions. The functions were originally designed to safely
    convert types from strings to integer types, but did no querying
    themselves.
    
    However, the naming scheme suggests they handle querying, so I've added
    that functionality in.
    
    This commit also fixes a bug where `errno` values within normal ranges
    were treated as error conditions.

--- src/env_config.c
+++ src/env_config.c
@@ -3,27 +3,29 @@
 
 #include "env_config.h"
 
-long env_get_long(const char* str, long default_value) {
+long env_get_long(const char* varname, long default_value) {
+    const char* str = getenv(varname);
     long out = default_value;
     // strtol segfaults if given a NULL ptr. Check before use!
     if (str != NULL) {
         out = strtol(str, NULL, 0);
     }
     // Error occurred in parsing, return default value.
-    if (errno != 0) {
+    if (errno == EINVAL || errno == ERANGE) {
         out = default_value;
     }
     return out;
 }
 
-unsigned long env_get_ulong(const char* str, unsigned long default_value) {
+unsigned long env_get_ulong(const char* varname, unsigned long default_value) {
+    const char* str = getenv(varname);
     unsigned long out = default_value;
     // strtoul segfaults if given a NULL ptr. Check before use!
     if (str != NULL) {
         out = strtoul(str, NULL, 0);
     }
     // Error occurred in parsing, return default value.
-    if (errno != 0) {
+    if (errno == EINVAL || errno == ERANGE) {
         out = default_value;
     }
     return out;
