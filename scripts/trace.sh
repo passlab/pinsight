e6dc9f06cf10e9432a53d5e295336513305343d1
Yonghong Yan <yanyh15@gmail.com>
2018-12-18 22:54:46 -0500

    modify the script and add a lulesh batch run script

--- scripts/trace.sh
+++ scripts/trace.sh
@@ -6,24 +6,25 @@
 USAGE=$(cat <<EOF
 Tracing script for use with PInsight.
 Usage:
-    trace.sh TRACEFILE_DEST OMP_LIB PINSIGHT_LIB ARGS...
+    trace.sh TRACEFILE_DEST PINSIGHT_LIB OMP_LIB_PATH OMP_NUM_THREADS ARGS...
 
 Arguments:
   TRACEFILE_DEST    Where to write the LTTng traces.
-  OMP_LIB           OpenMP shared library to use with user application.
-  PINSIGHT_LIB      PInsight shared library to use with user application.
+  PINSIGHT_LIB      PInsight shared library file name to use with user application.
+  OMP_LIB_PATH      OpenMP shared library path to use with user application.
+  OMP_NUM_THREADS   set this OpenMP env, i.e. number of OpenMP threads to use
 
 Examples:
     trace.sh /tmp/ompt-jacobi \\ 
-      /opt/openmp-install/lib/libomp.so \\ 
-      /opt/pinsight/lib/libvisuomp.so \\ 
+      /opt/pinsight/lib/libpinsight.so \\ 
+      /opt/openmp-install/lib 8 \\
       ./jacobi 2048 2048
 EOF
 )
 
 # Check to make sure user provided enough args.
 # If not, then exit early with usage info.
-if [ $# -lt 4 ]; then
+if [ $# -lt 5 ]; then
     echo "Error: Not enough arguments provided.";
     echo "${USAGE}";
     exit 1;
@@ -32,10 +33,11 @@ fi
 # Read in positional parameters, then shift array.
 # This leaves only the program command line in `$@`.
 TRACING_OUTPUT_DEST=$1
-OMP_LIB_PATH=$2
-PINSIGHT_LIB_PATH=$3
-shift 3
-
+PINSIGHT_LIB=$2
+OMP_LIB_PATH=$3
+export LD_LIBRARY_PATH=${OMP_LIB_PATH}:$LD_LIBRARY_PATH
+export OMP_NUM_THREADS=$4
+shift 4 
 
 # --------------------------------------------------------
 # Main script
@@ -50,7 +52,7 @@ lttng enable-event --userspace lttng_pinsight:'*'
 lttng start
 
 # Run instrumented code.
-LD_PRELOAD=${OMP_LIB_PATH}:${PINSIGHT_LIB_PATH} "$@"
+LD_PRELOAD=${PINSIGHT_LIB} "$@"
 
 # Stop LTTng tracing.
 lttng stop
