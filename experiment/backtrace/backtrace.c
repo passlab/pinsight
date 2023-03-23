bc41b2b99da53f34b99f4d079974ceb540b04700
Yonghong Yan <yanyh15@gmail.com>
2023-03-23 14:42:48 -0400

    experiment for backtrace and Dl_info for using addr2line

--- experiment/backtrace/backtrace.c
+++ experiment/backtrace/backtrace.c
@@ -1,6 +1,9 @@
+#define _GNU_SOURCE
+
 #include <execinfo.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <dlfcn.h>
 
 /* Obtain a backtrace and print it to stdout. */
 void
@@ -17,8 +20,16 @@ print_trace (void)
 
     printf ("Obtained %d stack frames.\n", size);
     for (i = 0; i < size; i++)
-      printf ("%s\n", strings[i]);
+      printf ("0x%p: %s\n", array[i], strings[i]);
   }
+
+  printf("------------------Dl_info --------------\n");
+  void * rt = __builtin_return_address(0);
+  Dl_info dlinfo;
+  dladdr(rt, &dlinfo);
+  printf("0x%p: offset: %p\n", rt, (void*)((unsigned long int) rt - (unsigned long int)dlinfo.dli_fbase));
+  printf("Dl_info: filename: %s, base address: %p, symbol: %s, symbol address: %p\n", 
+		  dlinfo.dli_fname, dlinfo.dli_fbase, dlinfo.dli_sname, dlinfo.dli_saddr);
 
   free (strings);
 }
