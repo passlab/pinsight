5feff78cbedbca1100ab8d00e8115f43389d6a8b
Yonghong Yan <yanyh15@gmail.com>
2023-10-08 23:00:58 -0400

    Making backtrace as a feature that can be enabled/disabled on build from cmake. The default is enabled. This commit also fix CUPTI build and CUDA support

--- experiment/lttng_cupti/lttng_cupti_tracepoint.h
+++ experiment/lttng_cupti/lttng_cupti_tracepoint.h
@@ -10,13 +10,13 @@
  * LTTng UST tracepoint definition for CUPTI callback API, so far, only cudaMemcpy, kernel launch and synchronization are provided
  *
  */
-#undef TRACEPOINT_PROVIDER
-#define TRACEPOINT_PROVIDER lttng_pinsight_cuda
+#undef LTTNG_UST_TRACEPOINT_PROVIDER
+#define LTTNG_UST_TRACEPOINT_PROVIDER lttng_pinsight_cuda
 
-#undef TRACEPOINT_INCLUDE_FILE
-#define TRACEPOINT_INCLUDE_FILE ./lttng_cupti_tracepoint.h
+#undef LTTNG_UST_TRACEPOINT_INCLUDE
+#define LTTNG_UST_TRACEPOINT_INCLUDE ./lttng_cupti_tracepoint.h
 
-#if !defined(LTTNG_CUPTI_TRACEPOINT_H) || defined(TRACEPOINT_HEADER_MULTI_READ)
+#if !defined(LTTNG_CUPTI_TRACEPOINT_H) || defined(LTTNG_UST_TRACEPOINT_HEADER_MULTI_READ)
 #define LTTNG_CUPTI_TRACEPOINT_H
 
 #ifdef __cplusplus
@@ -37,13 +37,13 @@ extern __thread int omp_thread_num;
 #endif
 #endif
 
-/** Macros used to simplify the definition of LTTng TRACEPOINT_EVENT */
+/** Macros used to simplify the definition of LTTng LTTNG_UST_TRACEPOINT_EVENT */
 #define CODEPTR_ARG \
     const void *, codeptr, \
     const char *, func_name
 
-//COMMON_TP_FIELDS_PMPI are those fields in the thread-local storage. These fields will be added to all the trace records
-#define COMMON_TP_FIELDS_MPI_OMP \
+//COMMON_LTTNG_UST_TP_FIELDS_PMPI are those fields in the thread-local storage. These fields will be added to all the trace records
+#define COMMON_LTTNG_UST_TP_FIELDS_MPI_OMP \
     ctf_integer(unsigned int, mpirank, mpirank) \
     ctf_integer(unsigned int, global_thread_num, global_thread_num) \
     ctf_integer(unsigned int, omp_thread_num, omp_thread_num) \
@@ -62,7 +62,7 @@ Device -> Device
         cudaMemcpyDefault = 4
 */
 TRACEPOINT_ENUM(lttng_pinsight_cuda, cudaMemcpyKind_enum,
-        TP_ENUM_VALUES(
+        LTTNG_UST_TP_ENUM_VALUES(
             ctf_enum_value("cudaMemcpyHostToHost", 0)
             ctf_enum_value("cudaMemcpyHostToDevice", 1)
             ctf_enum_value("cudaMemcpyDeviceToHost", 2)
@@ -72,18 +72,18 @@ TRACEPOINT_ENUM(lttng_pinsight_cuda, cudaMemcpyKind_enum,
 )
 
 /* for cudaMemcpy event */
-TRACEPOINT_EVENT(
+LTTNG_UST_TRACEPOINT_EVENT(
         lttng_pinsight_cuda,
         cudaMemcpy_begin,
-        TP_ARGS(
+        LTTNG_UST_TP_ARGS(
             CODEPTR_ARG,
             void *, dst,
             const void *, src,
             size_t, count,
             int, kind
         ),
-        TP_FIELDS(
-            COMMON_TP_FIELDS_MPI_OMP
+        LTTNG_UST_TP_FIELDS(
+            COMMON_LTTNG_UST_TP_FIELDS_MPI_OMP
             ctf_integer(unsigned int, dst, dst)
             ctf_integer(unsigned int, src, src)
             ctf_integer(unsigned int, count, count)
@@ -91,39 +91,39 @@ TRACEPOINT_EVENT(
         )
 )
 
-TRACEPOINT_EVENT(
+LTTNG_UST_TRACEPOINT_EVENT(
         lttng_pinsight_cuda,
         cudaMemcpy_end,
-        TP_ARGS(
+        LTTNG_UST_TP_ARGS(
             CODEPTR_ARG,
             int, return_val
         ),
-        TP_FIELDS(
-            COMMON_TP_FIELDS_MPI_OMP
+        LTTNG_UST_TP_FIELDS(
+            COMMON_LTTNG_UST_TP_FIELDS_MPI_OMP
             ctf_integer(int, return_val, return_val)
         )
 )
 
 /* for kernel launch event */
-TRACEPOINT_EVENT(
+LTTNG_UST_TRACEPOINT_EVENT(
         lttng_pinsight_cuda,
         cudaKernelLaunch_begin,
-        TP_ARGS(
+        LTTNG_UST_TP_ARGS(
             CODEPTR_ARG
         ),
-        TP_FIELDS(
-            COMMON_TP_FIELDS_MPI_OMP
+        LTTNG_UST_TP_FIELDS(
+            COMMON_LTTNG_UST_TP_FIELDS_MPI_OMP
         )
 )
 
-TRACEPOINT_EVENT(
+LTTNG_UST_TRACEPOINT_EVENT(
         lttng_pinsight_cuda,
         cudaKernelLaunch_end,
-        TP_ARGS(
+        LTTNG_UST_TP_ARGS(
             CODEPTR_ARG
         ),
-        TP_FIELDS(
-            COMMON_TP_FIELDS_MPI_OMP
+        LTTNG_UST_TP_FIELDS(
+            COMMON_LTTNG_UST_TP_FIELDS_MPI_OMP
         )
 )
 
