4cf501805e5c345a22d72acdfc51271e32d271c3
eliJI <elijahingr@gmail.com>
2023-10-17 17:09:13 -0400

    fix: added build folder to git ignore, implemented cudamemcpyasync functionality, changed cupti path to extra directory inside of local cuda install

--- src/cupti_callback.c
+++ src/cupti_callback.c
@@ -56,11 +56,18 @@ void CUPTIAPI CUPTI_callback_lttng(void *userdata, CUpti_CallbackDomain domain,
         return;
     }
     if (cbid == CUPTI_RUNTIME_TRACE_CBID_cudaMemcpyAsync_v3020) {
-        cudaMemcpyAsync_v3020_params *p = (cudaMemcpyAsync_v3020_params *) cbInfo->functionParams;
+        const char *funName = cbInfo -> functionName;
+       
         if (cbInfo->callbackSite == CUPTI_API_ENTER) {
-
+            cudaMemcpyAsync_v3020_params *p = (cudaMemcpyAsync_v3020_params *)(cbInfo->functionParams);
+            void *dst = p -> dst;
+            const void* src = p -> src;
+            unsigned int count = p -> count;
+            int kind = p -> kind;
+            lttng_ust_tracepoint(cupti_pinsight_lttng_ust, cudaMemcpy_begin, codeptr, funName, dst, src, count, kind);
         } else if (cbInfo->callbackSite == CUPTI_API_EXIT) {
-
+          int return_val = *((int *) cbInfo -> functionParams);
+          lttng_ust_tracepoint(cupti_pinsight_lttng_ust, cudaMemcpy_end, codeptr, funName, return_val);
         } else {
 
         }
