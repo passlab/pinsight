32ae3e6efd308449c9ba1b0ef767790f06300a2c
eliJI <elijahingr@gmail.com>
2023-11-03 16:45:29 -0400

    feat: added callback data struct to track ltting parameesr between callback functions

--- src/cupti_lttng_ust_tracepoint.h
+++ src/cupti_lttng_ust_tracepoint.h
@@ -103,6 +103,8 @@ LTTNG_UST_TRACEPOINT_EVENT(
             void *, dst,
             const void *, src,
             size_t, count,
+            int, stream,
+            int, cid,
             int, kind
         ),
         LTTNG_UST_TP_FIELDS(
@@ -110,6 +112,8 @@ LTTNG_UST_TRACEPOINT_EVENT(
             lttng_ust_field_integer(unsigned int, dst, dst)
             lttng_ust_field_integer(unsigned int, src, src)
             lttng_ust_field_integer(unsigned int, count, count)
+            lttng_ust_field_integer(unsigned int, stream, stream)
+            lttng_ust_field_integer(unsigned int, cid, cid)
             lttng_ust_field_enum(cupti_pinsight_lttng_ust, cudaMemcpyKind_enum, int, enumfield, kind)
         )
 )
