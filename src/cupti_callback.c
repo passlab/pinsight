bd8301f9bb4f710b122fb901c9e596c65d618d05
Yonghong Yan <yanyh15@gmail.com>
2023-11-29 20:43:43 -0500

    Now both contextId and streamId are recorded in the trace record. We use boxing to walk around the limitation of having max 10 args in the LTTNG_UST_TRACEPOINT_EVENT definition.

--- src/cupti_callback.c
+++ src/cupti_callback.c
@@ -48,8 +48,9 @@ void CUPTIAPI CUPTI_callback_lttng(void *userdata, CUpti_CallbackDomain domain,
             //printf("inside callback for kernelLaunch_begin: %s\n", kernelName);
             cudaStream_t stream = p->stream;
             unsigned int streamId; cuptiGetStreamIdEx(context, stream, 0, &streamId);
+            struct contextStreamId_t ctxStreamId; ctxStreamId.contextId = cxtId; ctxStreamId.streamId = streamId;
             uint64_t timeStamp; cuptiGetTimestamp(&timeStamp);
-            lttng_ust_tracepoint(cupti_pinsight_lttng_ust, cudaKernelLaunch_begin, devId, correlationId, timeStamp, codeptr, kernelName, streamId);
+            lttng_ust_tracepoint(cupti_pinsight_lttng_ust, cudaKernelLaunch_begin, devId, correlationId, timeStamp, codeptr, kernelName, &ctxStreamId);
         } else if (cbInfo->callbackSite == CUPTI_API_EXIT) {
             //printf("inside callback for kernelLaunch_end: %s\n", kernelName);
             uint64_t timeStamp; cuptiGetTimestamp(&timeStamp);
@@ -91,10 +92,13 @@ void CUPTIAPI CUPTI_callback_lttng(void *userdata, CUpti_CallbackDomain domain,
             unsigned int count = p->count;
             int kind = p->kind;
             cudaStream_t stream = p->stream;
-            unsigned int streamId; cuptiGetStreamIdEx(context, stream, 0, &streamId);
+            unsigned int streamId;
+            //cuptiGetStreamId(context, stream, &streamId);
+            cuptiGetStreamIdEx(context, stream, 0, &streamId);
+            struct contextStreamId_t ctxStreamId; ctxStreamId.contextId = cxtId; ctxStreamId.streamId = streamId;
+            //printf("cudaMemcpyAsync callback via CUPTI ContextID:StreamID: %d:%d, stream: %ld\n", cxtId, streamId, (long int)stream);
             uint64_t timeStamp; cuptiGetTimestamp(&timeStamp);
-            //printf("cudaMemcpyAsync ContextID:StreamID: %d:%d\n", cxtId, streamId);
-            lttng_ust_tracepoint(cupti_pinsight_lttng_ust, cudaMemcpyAsync_begin, devId, correlationId, timeStamp, codeptr, funName, dst, src, count, kind, streamId);
+            lttng_ust_tracepoint(cupti_pinsight_lttng_ust, cudaMemcpyAsync_begin, devId, correlationId, timeStamp, codeptr, funName, dst, src, count, kind, &ctxStreamId);
         } else if (cbInfo->callbackSite == CUPTI_API_EXIT) {
             int return_val = *((int *) cbInfo->functionReturnValue);
             uint64_t timeStamp; cuptiGetTimestamp(&timeStamp);
